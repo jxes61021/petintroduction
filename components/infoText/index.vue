df4666e1306cb728bd2406f42f81469a56feae8a
HUANG鉦順 <jxes6102@ceis.tw>
2022-08-04 14:32:04 +0800

    fix infotext

--- components/infoText/index.vue
+++ components/infoText/index.vue
@@ -1,28 +1,18 @@
-<template>
-  <div id="scrollBox" class="font-noto pb-4">
-    <p class="leading-2">
-      西元2469年，全球經濟全面瓦解，人類的生存已成死局，而人類也已經不再繁衍後代。人類遺傳學與生產權威艾瑞克博士為了拯救人類的命運，決定使用時光機回到2022年，把一次一次幫助人類渡過難關的科技神寵，用空投的方式降落回「古代」
-      <br />
-      <b class="text-4xl mt-4 block">「礦寵」</b>
-      <br />
-      祂們正在尋找天選之人。只要擁有祂，祂會幫助你找到有價值的礦產寶物...
-    </p>
-</div>
+<template lang="pug">
+div(class="flex flex-col items-center justify-center overflow-hidden text-center text-white font-noto pb-4")
+  div(v-for='(item, index) of message' :key="index" :class="item.isTitle ? 'text-4xl my-4 font-bold' : ''") {{item.text}}
 </template>
-<style scope>
-#scrollBox {
-  display: flex;
-  justify-content: center;
-	margin:0 auto;
-	overflow:hidden;
-  text-align: center;
-  color:white;
+<script>
+export default {
+  setup() {
+    const message = ref([
+      {isTitle: false, text: '西元2469年，全球經濟全面瓦解，人類的生存已成死局，而人類也已經不再繁衍後代。人類遺傳學與生產權威艾瑞克博士為了拯救人類的命運，決定使用時光機回到2022年，把一次一次幫助人類渡過難關的科技神寵，用空投的方式降落回「古代」'},
+      {isTitle: true, text: '「礦寵」'},
+      {isTitle: false, text: '祂們正在尋找天選之人。只要擁有祂，祂會幫助你找到有價值的礦產寶物...'}
+    ])
+    return{
+      message
+    }
+  }
 }
-#scrollBox #con1,#con2 {
-	float:left;
-}
-#scrollBox li {
-	text-align:left;
-	margin-bottom:20px
-}
-</style>
+</script>
