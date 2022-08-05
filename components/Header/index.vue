fafef532d62010f0c26786cdb7416edb228fae88
HUANG鉦順 <jxes6102@ceis.tw>
2022-08-05 11:25:58 +0800

    temp08051125

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,11 +1,9 @@
-<template>
-  <div class="w-full h-screen bg-moheaderImg md:bg-headerImg relative bg-no-repeat bg-cover" id="header">
-    <Navbar class="z-20 absolute" />
-    <div class="mine__top flex flex-col items-center justify-end h-full max-w-[800px] mx-auto p-[20px] md:p-0">
-      <img src="./image/i_logo_m2.svg" class="mb-0 md:mb-12 w-[187px] md:w-full mb-[20px] max-w-[490px]" />
-      <InfoText />
-    </div>
-  </div>
+<template lang="pug">
+div(class="w-full h-screen bg-moheaderImg md:bg-headerImg relative bg-no-repeat bg-cover" id="header")
+  Navbar(class="z-20 absolute")
+  div(class="mine__top flex flex-col items-center justify-end h-full max-w-[800px] mx-auto p-[20px] md:p-0")
+    img(src="./image/i_logo_m2.svg" class="mb-0 md:mb-12 w-[187px] md:w-full mb-[20px] max-w-[490px]")
+    InfoText
 </template>
 <script>
 export default {
